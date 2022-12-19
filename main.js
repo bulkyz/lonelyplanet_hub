5bc281356ebc2b007ab3acf84174700ec9998a7b
Vincent <77932350+bulkyz@users.noreply.github.com>
2022-12-19 16:15:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// de menu knoppen
+var navButtons = Array.from(document.querySelectorAll(".tab"));
+
+// listener als je op knop klikt
+for (navButton of navButtons) {
+  navButton.addEventListener("click", navClickHandler);
+}
+
+// maakt de knop waar je op klikt actief
+function navClickHandler(event) {
+  for (navButton of navButtons) {
+    navButton.classList.remove("active");
+  }
+  this.classList.add("active");
+}
